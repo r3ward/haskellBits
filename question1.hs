016954affd8a0d664013231c84084c349ac40b32
Richard Wardle <34130474+r3ward@users.noreply.github.com>
2020-11-05 10:17:28 +0000

    haskellGameSolver

--- question1.hs
+++ question1.hs
@@ -4,13 +4,13 @@ import Control.Monad
 convert :: (Integer, Integer, Integer, Integer, Integer, Integer) -> [Integer]
 convert (a, b, c, d, e, f) = [a, b, c, d, e, f]
 
-{- *** Question 1.1 *** -}
+
 rule1 :: (Integer, Integer, Integer, Integer, Integer, Integer) -> Bool
 rule1 x
  | (convert x) < nub (convert x) = False
  | otherwise = True
 
-{- *** Question 1.2 *** -}
+
 rule2method :: [Integer] -> Bool
 rule2method (x:[]) 
   = True
@@ -23,7 +23,7 @@ rule2 :: (Integer, Integer, Integer, Integer, Integer, Integer) -> Bool
 rule2 (xs)
  = rule2method(convert xs)
 
-{- *** Question 1.3 *** -}
+
 rule3method :: [Integer] -> Bool
 rule3method[] = True
 rule3method (x:[]) = True
@@ -35,16 +35,16 @@ rule3 :: (Integer, Integer, Integer, Integer, Integer, Integer) -> Bool
 rule3 (xs)
  = rule3method(convert xs)
 
-{- *** Question 1.4 *** -}
+
 rule4 :: (Integer, Integer, Integer, Integer, Integer, Integer) -> Bool
 rule4 (a, b, c, d, e, f)
     | mod (a*10+b) (e*10+f) == 0 && mod (c*10+d) (e*10+f) == 0 = True
     | otherwise = False
- 
-{- *** Question 1.5 *** -}
+
+
 possibles = fmap (\[a,b,c,d,e,f] -> (a,b,c,d,e,f)) . replicateM 6 $ [0..5]
 
-{- *** Question 1.6 *** -}
+
 solution:: (Integer, Integer, Integer, Integer, Integer, Integer) -> Bool
 solution xs
   | rule1 xs && rule2 xs && rule3 xs && rule4 xs = True
